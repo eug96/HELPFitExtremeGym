f627e71124da998f7bd636b50335b94b4149d751
alvin7285 <31263725+alvin7285@users.noreply.github.com>
2017-10-03 23:57:30 +0800

    Validation for SignUp trainer and memeber is done
    also adjusted the spacing of the code

--- javascript.js
+++ javascript.js
@@ -31,5 +31,62 @@ function TrainerLogin() {
     }
 
     location.replace("trainerPage.html")
+}
 
+function SignUpTrainer(){
+  var username = document.getElementById('inputUsername');
+  var password = document.getElementById('inputPassword');
+  var name = document.getElementById('inputName');
+  var email = document.getElementById('inputEmail');
+
+  if (username.value.length==0) {
+      alert("Please Enter Username");
+      return false;
+  }
+
+  else if (name.value.length==0) {
+    alert("Please Enter Name");
+    return false;
+  }
+
+  else if (email.value.length==0) {
+    alert("Please Enter Email");
+    return false;
+  }
+
+  else if (password.value.length==0) {
+    alert("Please Enter Password");
+    return false;
+  }
+
+  location.replace("TrainerLogin.html")
+}
+
+function SignUpMember(){
+  var username = document.getElementById('inputUsername');
+  var password = document.getElementById('inputPassword');
+  var name = document.getElementById('inputName');
+  var email = document.getElementById('inputEmail');
+
+  if (username.value.length==0) {
+      alert("Please Enter Username");
+      return false;
+  }
+
+  else if (name.value.length==0) {
+    alert("Please Enter Name");
+    return false;
+  }
+
+  else if (email.value.length==0) {
+    alert("Please Enter Email");
+    return false;
+  }
+
+  else if (password.value.length==0) {
+    alert("Please Enter Password");
+    return false;
+  }
+
+  location.replace("MemberLogin.html")
 }
