fef5344c7a4d1492505c3a96afc9a41a71cfc23b
alvin7285 <31263725+alvin7285@users.noreply.github.com>
2017-10-03 22:03:49 +0800

    Javascript file created
    The validation for member and trainer login page is done

---
+++ javascript.js
@@ -0,0 +1,35 @@
+function MemberLogin() {
+    var username = document.getElementById('inputUsername');
+    var password = document.getElementById('inputPassword');
+
+    if (username.value.length==0) {
+        alert("Please Enter Username");
+        return false;
+    }
+
+    else if (password.value.length==0) {
+      alert("Please Enter Password");
+      return false;
+    }
+
+    location.replace("memberPage.html")
+
+}
+
+function TrainerLogin() {
+    var username = document.getElementById('inputUsername');
+    var password = document.getElementById('inputPassword');
+
+    if (username.value.length==0) {
+        alert("Please Enter Username");
+        return false;
+    }
+
+    else if (password.value.length==0) {
+      alert("Please Enter Password");
+      return false;
+    }
+
+    location.replace("trainerPage.html")
+
+}
